3a9b14f1ae81572676bf399ab8d7d881fcb278f9
python019 <tommyjumayev@gmail.com>
2022-05-22 01:42:33 +0500

    UserCreationForm UserChangeForm

--- core/forms.py
+++ core/forms.py
@@ -5,7 +5,7 @@ from .models import CustomUser
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
-        fields = ('username', 'email', 'password')
+        fields = ('username', 'email')
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
