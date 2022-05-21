82c89d66e3d774521c20e323c75b127df72de482
python019 <tommyjumayev@gmail.com>
2022-05-22 03:17:26 +0500

    signin view

--- demo/urls.py
+++ demo/urls.py
@@ -2,11 +2,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from core.views import home
+from core.views import home,SigninView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", home, name="home"),
+    path("signin/", SigninView.as_view(), name="signin"),
     path('accounts/', include('django.contrib.auth.urls'))
 ]
 
