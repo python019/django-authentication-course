13f24f07d2b6da73de12b178e3f11aa16f9704e5
python019 <tommyjumayev@gmail.com>
2022-05-22 01:32:06 +0500

    UserCreationForm UserChangeForm

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,12 @@
 from msilib.schema import CustomAction
+from pyexpat import model
 from django.contrib import admin
 from .models import CustomUser
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 
-admin.site.register(CustomUser)
+class CustumUserAdmin(admin.ModelAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    list_display = ['username', 'email']
+            
+admin.site.register(CustomUser, CustumUserAdmin)
