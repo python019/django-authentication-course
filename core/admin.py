272aa1bdffa7d6d7152b75db64ab42e8b9f760b5
python019 <tommyjumayev@gmail.com>
2022-05-22 00:10:27 +0500

    04.AbstractUser_CustomUser

--- core/admin.py
+++ core/admin.py
@@ -1,1 +1,5 @@
+from msilib.schema import CustomAction
 from django.contrib import admin
+from .models import CustomUser
+
+admin.site.register(CustomUser)
--- core/models.py
+++ core/models.py
@@ -1,2 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, AbstractBaseUser
 
+class CustomUser(AbstractUser):
+    pass
